30b30c52fe75767769d146d223a5674ccbb65eb1
Messiovo <messiovo@gmail.com>
2022-09-06 13:27:13 +0100

    creating nav

---
+++ src/Page/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Nav } from '../Components'
+
+const Home = () => {
+  return (
+    <>
+        <Nav/>
+    </>
+  )
+}
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      light: "#CEE9B6",
+      medium: "#8FC79A",
+      dark: "#2D8D79",
+      dark2: "5E9387",
+    },
   },
   plugins: [],
-}
+};
